85c3e47fd08905500c08c1af387c4a0e83be2d02
katGhost <katghost124@gmail.com>
2026-04-21 01:50:25 +0200

    clean up completion

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,13 +19,12 @@ export const metadata = {
 
 
 
-export default function RootLayout(z) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body >
         <Navbar />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,65 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    // console.log(ctx);
+
+    function resize() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+
+    resize();
+    window.addEventListener("resize", resize);
+
+    function draw() {
+      const { width, height } = canvas;
+
+      // Create image data
+      const imageData = ctx.createImageData(width, height);
+      const data = imageData.data;
+
+      for (let y = 0; y < height; y += 2) {
+        for (let x = 0; x < width; x += 2) {
+          const i = (y * width + x) * 4;
+
+          // Base noise
+          const noise = Math.random() * 255;
+
+          // Halftone effect (grid-based threshold)
+          const pattern = ((x % 8) + (y % 8)) / 12;
+          const value = noise > pattern * 255 ? 255 : 0;
+
+          data[i] = value;  //red
+          data[i + 1] = 255;  //green
+          data[i + 2] = 0;  //blue
+          data[i + 3] = 150; // Alpha --> low for subtlety and high for more loudness
+        }
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+    }
+
+    // draw();
+    setInterval(draw, 100);
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className={styles.bgContainer}>
+            <canvas className={styles.bgCanvas}></canvas>
+            <div className={styles.bgOverlay}></div>
         </div>
       </main>
     </div>
