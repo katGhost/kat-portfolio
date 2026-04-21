4108ef58fd2919263895958d988232ddf76b1c60
katGhost <katghost124@gmail.com>
2026-04-21 11:38:21 +0200

    feat: add new index bypass canvas overlay -> navbar visibility

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
-// components/Navbar.tsx
+"use client";
+
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <Link href="/">Home</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      {/* Me */}
+      <div className={styles.titleContainer}>
+        <div className={styles.title}>
+          <span className={styles.logo}>AN</span>
+        </div>
+      </div>
+      {/* Navbar Links */}
+      <div className={styles.navLinksContainer}>
+        <div className={styles.navLinks}>
+          <Link className={styles.navItem} href="/">Home</Link>
+          <Link className={styles.navItem} href="/projects">Projects</Link>
+          <Link className={styles.navItem} href="/about">About</Link>
+          <Link className={styles.navItem} href="/contact">Contact</Link>
+        </div>
+      </div>
     </nav>
   );
 }
