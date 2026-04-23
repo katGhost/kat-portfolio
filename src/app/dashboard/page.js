c4ba3b8b2f0020dff4c00af85cf35cdc3d2f3dfd
katGhost <katghost124@gmail.com>
2026-04-23 10:41:30 +0200

    Introducing layer separation, separation of concerns

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Andries N. Mogashoa</h1>
+      </main>
+    </div>
+  );
+}
